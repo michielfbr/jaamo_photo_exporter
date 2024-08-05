3cc80a8e9d3c7534c80819b5a7eaa3a15b799e33
Michiel <michiel@valkdigital.nl>
2024-08-05 11:20:32 +0200

    Add: Extract current date from html

--- lib/jaamo_photo_exporter.exs
+++ lib/jaamo_photo_exporter.exs
@@ -22,9 +22,34 @@ defmodule JaamoPhotoExporter do
             {1, new_date}
 
           _ ->
-            :ok
+            {file_no + 1, current_date}
         end
       end)
+  end
+
+  defp get_date_from_item({"div", [{"class", "col-12 font_semi_bold text-start"}], [date]}) do
+    [day, month, year] = date |> String.trim() |> String.split(" ")
+    {:ok, "#{year}_#{map_month(month)}_#{day}"}
+  end
+
+  defp get_date_from_item(_), do: {:error, :no_date}
+
+  defp map_month("januari"), do: "01"
+  defp map_month("februari"), do: "02"
+  defp map_month("maart"), do: "03"
+  defp map_month("april"), do: "04"
+  defp map_month("mei"), do: "05"
+  defp map_month("juni"), do: "06"
+  defp map_month("juli"), do: "07"
+  defp map_month("augustus"), do: "08"
+  defp map_month("september"), do: "09"
+  defp map_month("oktober"), do: "10"
+  defp map_month("november"), do: "11"
+  defp map_month("december"), do: "12"
+  defp map_month(month), do: "#{month}"
+
+  defp print(input), do: [input] |> IO.ANSI.format() |> IO.puts()
+  defp print(input, formatting), do: [formatting, input] |> IO.ANSI.format() |> IO.puts()
 end
 
 JaamoPhotoExporter.run()
