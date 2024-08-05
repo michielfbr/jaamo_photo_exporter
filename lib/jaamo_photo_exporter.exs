0efaab702356c02be909be115bb3210b63169654
Michiel <michiel@valkdigital.nl>
2024-08-05 11:15:44 +0200

    Add: Extract current date from html

--- lib/jaamo_photo_exporter.exs
+++ lib/jaamo_photo_exporter.exs
@@ -11,6 +11,20 @@ defmodule JaamoPhotoExporter do
     [{"span", [], [page_title]}] = Floki.find(html, "span")
     base_folder = "output/#{page_title}"
     File.mkdir_p(base_folder)
+
+    [{"div", [{"class", "image_gallery d-flex flex-wrap mx-2"}], gallery_items}] =
+      Floki.find(html, ".image_gallery")
+
+    {_file_no, _current_date} =
+      Enum.reduce(gallery_items, {1, ""}, fn item, {file_no, current_date} ->
+        case get_date_from_item(item) do
+          {:ok, new_date} ->
+            {1, new_date}
+
+          _ ->
+            :ok
+        end
+      end)
 end
 
 JaamoPhotoExporter.run()
