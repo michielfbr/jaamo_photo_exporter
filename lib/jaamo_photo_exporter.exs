dd4c5ad5ea76febac6ec14177c7aa8510295f3b9
Michiel <michiel@valkdigital.nl>
2024-08-05 11:18:38 +0200

    Add: print to terminal

--- lib/jaamo_photo_exporter.exs
+++ lib/jaamo_photo_exporter.exs
@@ -1,24 +1,34 @@
 defmodule JaamoPhotoExporter do
   @moduledoc false
   def run() do
-    Mix.install([{:floki, "~> 0.36.2"}])
+    print("Jaamo image export", :blue_background)
+
+    print("Installing dependencies")
+
+    print("Reading input file")
 
     {:ok, html} =
       System.get_env("INPUT_FILE", "input/photos.html")
       |> File.read!()
       |> Floki.parse_document()
 
+    print("Creating output folder")
     [{"span", [], [page_title]}] = Floki.find(html, "span")
     base_folder = "output/#{page_title}"
     File.mkdir_p(base_folder)
 
+    print("Starting export for: #{page_title}", :green)
+
     [{"div", [{"class", "image_gallery d-flex flex-wrap mx-2"}], gallery_items}] =
       Floki.find(html, ".image_gallery")
+
+    print("Saving images...")
 
     {_file_no, _current_date} =
       Enum.reduce(gallery_items, {1, ""}, fn item, {file_no, current_date} ->
         case get_date_from_item(item) do
           {:ok, new_date} ->
+            print("-- #{new_date} --", :green)
             {1, new_date}
 
           _ ->
