c736aa35bab3bf6124c59bedf62cfbd8204faf47
Michiel <michiel@valkdigital.nl>
2024-08-05 11:12:47 +0200

    Add: Read html & create output folder

--- lib/jaamo_photo_exporter.exs
+++ lib/jaamo_photo_exporter.exs
@@ -1,8 +1,16 @@
 defmodule JaamoPhotoExporter do
   @moduledoc false
   def run() do
-    dbg("Run forrest!")
-  end
+    Mix.install([{:floki, "~> 0.36.2"}])
+
+    {:ok, html} =
+      System.get_env("INPUT_FILE", "input/photos.html")
+      |> File.read!()
+      |> Floki.parse_document()
+
+    [{"span", [], [page_title]}] = Floki.find(html, "span")
+    base_folder = "output/#{page_title}"
+    File.mkdir_p(base_folder)
 end
 
 JaamoPhotoExporter.run()
